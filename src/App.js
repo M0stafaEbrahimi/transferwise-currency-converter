800f9f2955de5b066d43dea3fd2b49e23c2d0a43
M0stafaEbrahimi <mse.researcher@gmail.com>
2022-02-12 13:51:51 +0330

    implement all functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "@transferwise/icons/lib/styles/main.min.css";
 import "@transferwise/components/build/main.css";
 import "../node_modules/currency-flags/dist/currency-flags.min.css";
 import "./App.css";
+import { useEffect, useState } from "react";
 import {
   getLangFromLocale,
   DEFAULT_LOCALE,
@@ -10,87 +11,113 @@ import {
   MoneyInput,
   translations as componentTranslations,
 } from "@transferwise/components";
-
+const BASE_URL =
+  "http://api.exchangeratesapi.io/v1/latest?access_key=c5debda6ee0fb802489c0e7d10ad5eba&format=1";
 function App() {
+  const [currencies, setCurrencies] = useState([
+    {
+      header: "Popular currencies",
+    },
+    {
+      value: "EUR",
+      label: "EUR",
+      currency: "eur",
+    },
+    {
+      value: "GBP",
+      label: "GBP",
+      currency: "gbp",
+    },
+    {
+      header: "All currencies",
+    },
+  ]);
+  console.log(currencies);
+  const [fromCurrency, setFromCurrency] = useState({
+    value: "EUR",
+    label: "EUR",
+    currency: "eur",
+  });
+  const [toCurrency, setToCurrency] = useState({
+    value: "GBP",
+    label: "GBP",
+    currency: "gbp",
+  });
+  const [fromAmount, setFromAmount] = useState(1);
+  const [toAmount, setToAmount] = useState(1);
+  const [rates, setRates] = useState();
   const lang = getLangFromLocale(DEFAULT_LOCALE);
   const messages = { ...componentTranslations[lang] };
+  function handleAmount1Change(amount1) {
+    setFromAmount(amount1);
+    setToAmount(
+      (amount1 * rates[toCurrency.value]) / rates[fromCurrency.value]
+    );
+  }
+  function handleCurrency1Change(currency1) {
+    setToAmount(
+      (fromAmount * rates[toCurrency.value]) / rates[currency1.value]
+    );
+    setFromCurrency(currency1);
+  }
+  function handleAmount2Change(amount2) {
+    setToAmount(amount2);
+    setFromAmount(
+      (amount2 * rates[fromCurrency.value]) / rates[toCurrency.value]
+    );
+  }
+  function handleCurrency2Change(currency2) {
+    setFromAmount(
+      (toAmount * rates[fromCurrency.value]) / rates[currency2.value]
+    );
+    setToCurrency(currency2);
+  }
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const currencyOptions = Object.keys(data.rates);
+        //const firstCurrency = Object.keys(data.rates)[0];
+        const currData = currencyOptions.map((opt) => ({
+          value: opt,
+          label: opt,
+          currency: opt.toLowerCase(),
+        }));
+        setCurrencies((prev) => [...prev, ...currData]);
+        setRates(data.rates);
+      });
+  }, []);
+  useEffect(() => {
+    if (!!rates) {
+      handleAmount1Change(1);
+    }
+  }, [rates]);
   return (
     <Provider i18n={{ locale: DEFAULT_LOCALE, messages }}>
       <h1>Convertor</h1>
       <MoneyInput
         id="money-input"
-        amount={1000}
+        amount={fromAmount}
         size="lg"
-        onAmountChange={(value) => console.log("amount changed", value)}
-        onCurrencyChange={() => alert("currency changed")}
+        onAmountChange={handleAmount1Change}
+        onCurrencyChange={handleCurrency1Change}
         addon={null}
-        searchPlaceholder="Type a currency or country"
-        onCustomAction={() => alert("Custom action")}
-        customActionLabel="Custom action label"
-        currencies={[
-          {
-            header: "Popular currencies",
-          },
-          {
-            value: "EUR",
-            label: "EUR",
-            note: "Euro",
-            currency: "eur",
-            searchable: "Spain, Germany, France, Austria",
-          },
-          {
-            value: "GBP",
-            label: "GBP",
-            note: "British pound",
-            currency: "gbp",
-            searchable: "England, Scotland, Wales",
-          },
-        ]}
-        selectedCurrency={{
-          value: "EUR",
-          label: "EUR",
-          note: "Euro",
-          currency: "eur",
-          searchable: "Spain, Germany, France, Austria",
-        }}
+        searchPlaceholder="Type a currency"
+        currencies={currencies}
+        selectedCurrency={fromCurrency}
       />
       <h2>=</h2>
       <MoneyInput
         id="money-input"
-        amount={1000}
+        amount={toAmount}
         size="lg"
-        onAmountChange={(value) => console.log("amount changed", value)}
-        onCurrencyChange={() => alert("currency changed")}
+        onAmountChange={handleAmount2Change}
+        onCurrencyChange={handleCurrency2Change}
         addon={null}
-        searchPlaceholder="Type a currency or country"
-        onCustomAction={() => alert("Custom action")}
-        customActionLabel="Custom action label"
-        currencies={[
-          {
-            header: "Popular currencies",
-          },
-          {
-            value: "EUR",
-            label: "EUR",
-            note: "Euro",
-            currency: "eur",
-            searchable: "Spain, Germany, France, Austria",
-          },
-          {
-            value: "GBP",
-            label: "GBP",
-            note: "British pound",
-            currency: "gbp",
-            searchable: "England, Scotland, Wales",
-          },
-        ]}
-        selectedCurrency={{
-          value: "EUR",
-          label: "EUR",
-          note: "Euro",
-          currency: "eur",
-          searchable: "Spain, Germany, France, Austria",
-        }}
+        searchPlaceholder="Type a currency"
+        currencies={currencies}
+        selectedCurrency={toCurrency}
       />
     </Provider>
   );
