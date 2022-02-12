6bc8712854f21f5771c355f97ee8daeac188fd46
M0stafaEbrahimi <mse.researcher@gmail.com>
2022-02-12 11:43:35 +0330

    add transferwise packages and add moneyinput component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import "@transferwise/neptune-css/dist/css/neptune.css";
+import "@transferwise/icons/lib/styles/main.min.css";
+import "@transferwise/components/build/main.css";
+import "../node_modules/currency-flags/dist/currency-flags.min.css";
+import "./App.css";
+import {
+  getLangFromLocale,
+  DEFAULT_LOCALE,
+  Provider,
+  MoneyInput,
+  translations as componentTranslations,
+} from "@transferwise/components";
 
 function App() {
+  const lang = getLangFromLocale(DEFAULT_LOCALE);
+  const messages = { ...componentTranslations[lang] };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider i18n={{ locale: DEFAULT_LOCALE, messages }}>
+      <h1>Convertor</h1>
+      <MoneyInput
+        id="money-input"
+        amount={1000}
+        size="lg"
+        onAmountChange={(value) => console.log("amount changed", value)}
+        onCurrencyChange={() => alert("currency changed")}
+        addon={null}
+        searchPlaceholder="Type a currency or country"
+        onCustomAction={() => alert("Custom action")}
+        customActionLabel="Custom action label"
+        currencies={[
+          {
+            header: "Popular currencies",
+          },
+          {
+            value: "EUR",
+            label: "EUR",
+            note: "Euro",
+            currency: "eur",
+            searchable: "Spain, Germany, France, Austria",
+          },
+          {
+            value: "GBP",
+            label: "GBP",
+            note: "British pound",
+            currency: "gbp",
+            searchable: "England, Scotland, Wales",
+          },
+        ]}
+        selectedCurrency={{
+          value: "EUR",
+          label: "EUR",
+          note: "Euro",
+          currency: "eur",
+          searchable: "Spain, Germany, France, Austria",
+        }}
+      />
+      <h2>=</h2>
+      <MoneyInput
+        id="money-input"
+        amount={1000}
+        size="lg"
+        onAmountChange={(value) => console.log("amount changed", value)}
+        onCurrencyChange={() => alert("currency changed")}
+        addon={null}
+        searchPlaceholder="Type a currency or country"
+        onCustomAction={() => alert("Custom action")}
+        customActionLabel="Custom action label"
+        currencies={[
+          {
+            header: "Popular currencies",
+          },
+          {
+            value: "EUR",
+            label: "EUR",
+            note: "Euro",
+            currency: "eur",
+            searchable: "Spain, Germany, France, Austria",
+          },
+          {
+            value: "GBP",
+            label: "GBP",
+            note: "British pound",
+            currency: "gbp",
+            searchable: "England, Scotland, Wales",
+          },
+        ]}
+        selectedCurrency={{
+          value: "EUR",
+          label: "EUR",
+          note: "Euro",
+          currency: "eur",
+          searchable: "Spain, Germany, France, Austria",
+        }}
+      />
+    </Provider>
   );
 }
 
